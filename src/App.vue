1fd92043b43261ac34171d413d3a5f5e0c9c4bca
Rodrigo Valdés <rodrigovaldes@gmail.com>
2020-10-29 01:39:41 -0300

    Actualiza textos de sitio.

--- src/App.vue
+++ src/App.vue
@@ -108,6 +108,116 @@
         <img class="h-56 w-full object-cover sm:h-72 md:h-96 lg:w-full lg:h-full" src="https://images.unsplash.com/photo-1551434678-e076c223a692?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=2850&q=80" alt="">
       </div>
     </div>
+    <!--
+  Tailwind UI components require Tailwind CSS v1.8 and the @tailwindcss/ui plugin.
+  Read the documentation to get started: https://tailwindui.com/documentation
+-->
+    <div class="py-12 bg-white">
+      <div class="max-w-screen-xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div class="lg:text-center">
+          <p class="text-base leading-6 text-indigo-600 font-semibold tracking-wide uppercase">High Performance IT</p>
+          <h3 class="mt-2 text-3xl leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl sm:leading-10">
+            Servicio de Consultoría de Tecnologías de Información
+          </h3>
+          <p class="mt-4 max-w-2xl text-xl leading-7 text-gray-500 lg:mx-auto">
+            El servicio de consultoría de Cybersyn es Altamente especializada y el objetivo es proveer el servicio de la más
+            alta calidad gracias a la aplicación de mejora continua e innovación continua.
+          </p>
+        </div>
+
+        <div class="mt-10">
+          <ul class="md:grid md:grid-cols-2 md:gap-x-8 md:gap-y-10">
+            <li>
+              <div class="flex">
+                <div class="flex-shrink-0">
+                  <div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                    <!-- Heroicon name: globe-alt -->
+                    <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 12a9 9 0 01-9 9m9-9a9 9 0 00-9-9m9 9H3m9 9a9 9 0 01-9-9m9 9c1.657 0 3-4.03 3-9s-1.343-9-3-9m0 18c-1.657 0-3-4.03-3-9s1.343-9 3-9m-9 9a9 9 0 019-9" />
+                    </svg>
+                  </div>
+                </div>
+                <div class="ml-4">
+                  <h4 class="text-lg leading-6 font-medium text-gray-900">Camino a la nube</h4>
+                  <p class="mt-2 text-base leading-6 text-gray-500">
+                    Aprovecha las ventajas de la nube. Preparamos un detallado plan de migración de aplicaciones a la nube.
+                  </p>
+                </div>
+              </div>
+            </li>
+            <li class="mt-10 md:mt-0">
+              <div class="flex">
+                <div class="flex-shrink-0">
+                  <div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                    <!-- Heroicon name: scale -->
+                    <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 6l3 1m0 0l-3 9a5.002 5.002 0 006.001 0M6 7l3 9M6 7l6-2m6 2l3-1m-3 1l-3 9a5.002 5.002 0 006.001 0M18 7l3 9m-3-9l-6-2m0-2v2m0 16V5m0 16H9m3 0h3" />
+                    </svg>
+                  </div>
+                </div>
+                <div class="ml-4">
+                  <h4 class="text-lg leading-6 font-medium text-gray-900">Atlassian Experts</h4>
+                  <p class="mt-2 text-base leading-6 text-gray-500">
+                    <!--Lorem ipsum, dolor sit amet consectetur adipisicing elit. Maiores impedit perferendis suscipit eaque, iste dolor cupiditate blanditiis ratione.-->
+                    Implementación de ciclo de desarrollo End 2 end integrando Herramientas Atlassian. Jira, Confluence, Bitbucket.
+                  </p>
+                </div>
+              </div>
+            </li>
+            <li class="mt-10 md:mt-0">
+              <div class="flex">
+                <div class="flex-shrink-0">
+                  <div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                    <!-- Heroicon name: lightning-bolt -->
+                    <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M13 10V3L4 14h7v7l9-11h-7z" />
+                    </svg>
+                  </div>
+                </div>
+                <div class="ml-4">
+                  <h4 class="text-lg leading-6 font-medium text-gray-900">DevOps</h4>
+                  <p class="mt-2 text-base leading-6 text-gray-500">
+                    Te ayudamos con una evaluación y la generación de un plan de acción para implementación de practicas devops.
+                  </p>
+                </div>
+              </div>
+            </li>
+            <li class="mt-10 md:mt-0">
+              <div class="flex">
+                <div class="flex-shrink-0">
+                  <div class="flex items-center justify-center h-12 w-12 rounded-md bg-indigo-500 text-white">
+                    <!-- Heroicon name: annotation -->
+                    <svg class="h-6 w-6" xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                      <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M7 8h10M7 12h4m1 8l-4-4H5a2 2 0 01-2-2V6a2 2 0 012-2h14a2 2 0 012 2v8a2 2 0 01-2 2h-3l-4 4z" />
+                    </svg>
+                  </div>
+                </div>
+                <div class="ml-4">
+                  <h4 class="text-lg leading-6 font-medium text-gray-900">Innovación Continua</h4>
+                  <p class="mt-2 text-base leading-6 text-gray-500">
+                    Te asesoramos en la implementación de metodologías que aceleran la capacidad de innovar y poder adaptar asi tu negocio a los desafios.
+                  </p>
+                </div>
+              </div>
+            </li>
+          </ul>
+        </div>
+
+
+
+        <div class="md:flex py-12">
+          <div class="md:flex-shrink-0">
+            <img class="rounded-lg md:w-56" src="https://images.unsplash.com/photo-1556740738-b6a63e27c4df?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=448&q=80" alt="Woman paying for a purchase">
+          </div>
+          <div class="mt-4 md:mt-0 md:ml-6">
+            <div class="uppercase tracking-wide text-sm text-indigo-600 font-bold">DevSecOps</div>
+            <a href="#" class="block mt-1 text-lg leading-tight font-semibold text-gray-900 hover:underline">Finding customers for your new business</a>
+            <p class="mt-2 text-gray-600">Getting a new business off the ground is a lot of hard work. Here are five ideas you can use to find your first customers.</p>
+          </div>
+        </div>
+      </div>
+    </div>
+
 
   </div>
 </template>
